2840b4828a366db2d791f6143a071d1ba04af362
Zamzam <zamzamalabri18@gmail.com>
2023-08-08 11:57:35 +0400

    end of section 2

---
+++ API/Data/Migrations/20230807061601_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users", //name table
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true), //column 1
+                    UserName = table.Column<string>(type: "TEXT", nullable: true) //column 2
+                },
+                constraints: table =>
+                {
+                    //primaryKey
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    //entity framework --> acess modifer must be public
+    public int Id { get; set; }
+
+    public string UserName {get;set;}
+
+}
